5cbed6dc1f0339c719a43fda2e86b9209105a898
anurupmajumder94 <109573820+anurupmajumder94@users.noreply.github.com>
2025-08-29 00:16:03 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="Agentic RAG", layout="wide")
+
+# Title Section
+st.title("Agentic RAG for Autonomous Incident Resolution in Enterprise IT Systems")
+st.subheader("Anurup Majumder")
